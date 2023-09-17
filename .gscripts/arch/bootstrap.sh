201f45bae3d1401f19cbe4af5d7b3dfc1e0e08e1
Gerry Hernandez <gerry@gerryhernandez.com>
2023-09-17 02:16:38 -0700

    Various tweaks

--- .gscripts/arch/bootstrap.sh
+++ .gscripts/arch/bootstrap.sh
@@ -20,7 +20,6 @@ paci ccache              # Compiler Cache to speed up some compilations
 paci clang               # Compiler for C-family languages
 paci cmake               # Configurable cross platform make
 paci dfu-util            # DFU-mode firmware flashing for keyboard
-paci discord             # Like IRC, only worse
 paci fd                  # Alternative to find
 paci figlet              # ASCII art text generator
 paci fzf                 # TUI/CLI fuzzy finder
@@ -30,35 +29,49 @@ paci github-cli          # GitHub CLI
 paci go                  # A gopher who lives on land above C level
 paci gopls               # A very polite languang server for Go
 paci hyprland            # Tiling window manager for Wayland
+paci hyprpaper           # Wallpaper util
 paci llvm                # Next-gen compiler backend
 paci lolcat              # The purrfect addition to the CLI
 paci lua-language-server # As the name implies
 paci luarocks            # Package manager for Lua
 paci man-db              # Manual pages
 paci marksman            # Markdown language server
+paci neofetch            # necessary for internet points
 paci ncurses             # TUI tooling
 paci neovim              # NeoVim terminal-based text editor
 paci nodejs              # "JavaScript... on a SeRvEr!?!?"
 paci noto-fonts-emoji    # Emoji fonts from Google
 paci npm                 # Defitely not node package manager
 paci pacman-contrib      # For building AUR related things
+paci pipewire            # Audio stack
+paci polkit-kde-agent    # Essentially a "sudo GUI" for gui apps
 paci python-pip          # Package manager for Python
 paci python3             # A snake that lives on land above C level
+paci qt5-wayland         # QT5 lib
+paci qt6-wayland         # QT6 lib
 paci ripgrep             # Fast search tool
 paci rust                # Crabs
 paci sddm                # Display/Login/Session manager
 paci shfmt               # Formats shell programs
 paci stylua              # Lua formatter
+paci timeshift           # BTRFS snapshot easymode
 paci tldr                # Short alternative to man-pages
 paci tmux                # Terminal Multiplexer
 paci tree                # Show directory structure visually
+paci ttf-liberation      # OSS compatible Microsoft fonts
 paci wget                # Gets things from the web
 paci which               # Doesn't show you the way, but shows you a path
+paci wireplumber         # Audio patching
 paci wofi                # Launcher
 paci xdg-utils           # Desktop integrations
 paci yarn                # Package manager for Node
 paci zip                 # Technologic
 paci zsh                 # Interactive shell of choice
+
+paci xdg-desktop-portal-hyprland # hyprland-specific XDG Portals
+
+# Control monitor input programmatically
+paci ddcutil
 
 # for ZMK keyboard development
 paci dtc   # Device Tree Controller
@@ -91,8 +104,10 @@ fi
 function auri() {
 	paru -S --needed --noconfirm "$@"
 }
-auri brave-bin # A web browser that isn't scared of privacy
-auri zoom      # Mazda's software division
+auri brave-bin      # A web browser that isn't scared of privacy
+auri hyprpicker-git # Pixel color picker
+auri webcord-bin    # A better Discord client
+auri zoom           # Mazda's software division
 
 # Language-specific packages:
 sudo luarocks install luacheck
@@ -130,3 +145,7 @@ timedatectl set-timezone "$(curl --fail https://ipapi.co/timezone)"
 pushd ~/.config/alacritty
 ./init.sh
 popd
+
+#{{{ Steam
+paci lib32-mesa # 32 bit OpenGL for AMD GPUs
+#}}}
