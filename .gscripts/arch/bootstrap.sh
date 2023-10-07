174a2cecd81d4b01c18000a4adc81d2a9e7ce786
Gerry Hernandez <gerry@gerryhernandez.com>
2023-10-07 00:19:28 -0700

    Improve Arch bootstrap script

--- .gscripts/arch/bootstrap.sh
+++ .gscripts/arch/bootstrap.sh
@@ -9,112 +9,108 @@ sudo pacman -Syu --noconfirm
 # "pacman install"
 function paci() {
 	sudo pacman -S --needed --noconfirm "$@"
+	packages=""
 }
-paci alacritty           # The stubbornly non-sixel terminal emulator
-paci atuin               # CLI history in searchable sqlite
-paci base-devel          # Base development packages
-paci bash                # What you shouldn't do until you try it
-paci bat                 # Better cat
-paci bear                # Generates compile databases for clangd
-paci cargo               # Rust package manager
-paci ccache              # Compiler Cache to speed up some compilations
-paci clang               # Compiler for C-family languages
-paci cmake               # Configurable cross platform make
-paci dfu-util            # DFU-mode firmware flashing for keyboard
-paci fd                  # Alternative to find
-paci figlet              # ASCII art text generator
-paci fzf                 # TUI/CLI fuzzy finder
-paci gcc                 # C compiler
-paci git                 # Git version control
-paci github-cli          # GitHub CLI
-paci go                  # A gopher who lives on land above C level
-paci gopls               # A very polite languang server for Go
-paci jq                  # Manipulate JSON in the CLI
-paci kubectl             # Some people say "cube cuddle"
-paci llvm                # Next-gen compiler backend
-paci lolcat              # The purrfect addition to the CLI
-paci lua-language-server # As the name implies
-paci luarocks            # Package manager for Lua
-paci man-db              # Manual pages
-paci marksman            # Markdown language server
-paci neofetch            # necessary for internet points
-paci ncurses             # TUI tooling
-paci neovim              # NeoVim terminal-based text editor
-paci nodejs              # "JavaScript... on a SeRvEr!?!?"
-paci noto-fonts-emoji    # Emoji fonts from Google
-paci npm                 # Defitely not node package manager
-paci pacman-contrib      # For building AUR related things
-paci pipewire            # Audio stack
-paci python-pipx         # Install Python packages with automagic virtualenv
-paci polkit-kde-agent    # Essentially a "sudo GUI" for gui apps
-paci python-pip          # Package manager for Python
-paci python3             # A snake that lives on land above C level
-paci qt5-wayland         # QT5 lib
-paci qt6-wayland         # QT6 lib
-paci ripgrep             # Fast search tool
-paci rust                # Crabs
-paci sddm                # Display/Login/Session manager
-paci shfmt               # Formats shell programs
-paci stylua              # Lua formatter
-paci timeshift           # BTRFS snapshot easymode
-paci tldr                # Short alternative to man-pages
-paci tmux                # Terminal Multiplexer
-paci tree                # Show directory structure visually
-paci ttf-liberation      # OSS compatible Microsoft fonts
-paci wget                # Gets things from the web
-paci which               # Doesn't show you the way, but shows you a path
-paci wireplumber         # Audio patching
-paci wofi                # Launcher
-paci xdg-utils           # Desktop integrations
-paci yarn                # Package manager for Node
-paci zip                 # Technologic
-paci zsh                 # Interactive shell of choice
 
-# Control monitor input programmatically
-paci ddcutil
+# "package append"
+packages=""
+function paca() {
+	packages="$packages $@"
+}
+
+paca alacritty           # The stubbornly non-sixel terminal emulator
+paca atuin               # CLI history in searchable sqlite
+paca base-devel          # Base development packages
+paca bash                # What you shouldn't do until you try it
+paca bat                 # Better cat
+paca bear                # Generates compile databases for clangd
+paca cargo               # Rust package manager
+paca ccache              # Compiler Cache to speed up some compilations
+paca clang               # Compiler for C-family languages
+paca cmake               # Configurable cross platform make
+paca chromium            # For surfing
+paca dfu-util            # DFU-mode firmware flashing for keyboard
+paca ddcutil             # Control monitor input programmatically
+paca fd                  # Alternative to find
+paca figlet              # ASCII art text generator
+paca fzf                 # TUI/CLI fuzzy finder
+paca gcc                 # C compiler
+paca git                 # Git version control
+paca github-cli          # GitHub CLI
+paca go                  # A gopher who lives on land above C level
+paca gopls               # A very polite languang server for Go
+paca jq                  # Manipulate JSON in the CLI
+paca kubectl             # Some people say "cube cuddle"
+paca llvm                # Next-gen compiler backend
+paca lolcat              # The purrfect addition to the CLI
+paca lua-language-server # As the name implies
+paca luarocks            # Package manager for Lua
+paca man-db              # Manual pages
+paca marksman            # Markdown language server
+paca neofetch            # necessary for internet points
+paca ncurses             # TUI tooling
+paca neovim              # NeoVim terminal-based text editor
+paca nodejs              # "JavaScript... on a SeRvEr!?!?"
+paca noto-fonts-emoji    # Emoji fonts from Google
+paca npm                 # Defitely not node package manager
+paca pacman-contrib      # For building AUR related things
+paca pipewire            # Audio stack
+paca python-pipx         # Install Python packages with automagic virtualenv
+paca polkit-kde-agent    # Essentially a "sudo GUI" for gui apps
+paca python-pip          # Package manager for Python
+paca python3             # A snake that lives on land above C level
+paca qt5-wayland         # QT5 lib
+paca qt6-wayland         # QT6 lib
+paca ripgrep             # Fast search tool
+paca rust                # Crabs
+paca sddm                # Display/Login/Session manager
+paca shfmt               # Formats shell programs
+paca stylua              # Lua formatter
+paca timeshift           # BTRFS snapshot easymode
+paca tldr                # Short alternative to man-pages
+paca tmux                # Terminal Multiplexer
+paca tree                # Show directory structure visually
+paca ttf-liberation      # OSS compatible Microsoft fonts
+paca wget                # Gets things from the web
+paca which               # Doesn't show you the way, but shows you a path
+paca wireplumber         # Audio patching
+paca wofi                # Launcher
+paca xdg-utils           # Desktop integrations
+paca yarn                # Package manager for Node
+paca zip                 # Technologic
+paca zsh                 # Interactive shell of choice
+
+paci $packages
 
 # for ZMK keyboard development
 paci dtc   # Device Tree Controller
 paci ninja # Buid system for CMake
-
-# for QMK keyboard development:
-paci \
-	qmk \
-	arm-none-eabi-binutils \
-	arm-none-eabi-gcc \
-	arm-none-eabi-newlib \
-	avr-binutils \
-	avr-gcc \
-	avr-libc \
-	dfu-programmer
-
-# for ZMK keyboard development
 pipx install west
 
-# Install Paru AUR helper from source
-mkdir -p src/aur.archlinux.org
-pushd src/aur.archlinux.org
-
-# if the ./paru directory exists, exit
-if [ ! -d "paru" ]; then
-	git clone https://aur.archlinux.org/paru.git
-	cd paru
-	makepkg -si --noconfirm
+# install yay (aur helper) if it isn't already installed
+if ! [ -x "$(command -v yay)" ]; then
+	pacman -S --needed git base-devel
+	pushd ~/
+	mkdir -p src/aur.archlinux.org
+	cd src/aur.archlinux.org
+	git clone https://aur.archlinux.org/yay.git
+	cd yay
+	makepkg -si
 	popd
 fi
 
 # "aur install"
 function auri() {
-	paru -S --needed --noconfirm "$@"
+	yay -S --needed --noconfirm "$@"
 }
-auri brave-bin      # A web browser that isn't scared of privacy
-auri hyprpicker-git # Pixel color picker
-auri webcord-bin    # A better Discord client
-auri zoom           # Mazda's software division
+auri webcord-bin # A better Discord client
+auri zoom        # Mazda's software division
 
 # Language-specific packages:
 sudo luarocks install luacheck
-go install github.com/go-delve/delve/cmd/dlv@latest
+if ! [ -x "$(command -v dlv)" ]; then
+	go install github.com/go-delve/delve/cmd/dlv@latest
+fi
 
 # Fonts for a good terminal experience
 sudo mkdir -p /usr/local/share/fonts/nerd
@@ -146,7 +142,6 @@ timedatectl set-timezone "$(curl --fail https://ipapi.co/timezone)"
 
 # Init Alacritty config
 pushd ~/.config/alacritty
-ln -s linux.yml alacritty.yml
 cat <<EOF >alacritty.yml
 import:
   - ~/.config/alacritty/linux.yml
