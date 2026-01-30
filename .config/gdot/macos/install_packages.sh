3a740925f5eae4510f2c47e573760a0374cac835
Gerry Hernandez <gerrhernandez@ebay.com>
2026-01-29 17:09:39 -0800

    Switch to Dwmac, remove Amethyst

--- .config/gdot/macos/install_packages.sh
+++ .config/gdot/macos/install_packages.sh
@@ -7,11 +7,5 @@ fi
 
 cd "$(dirname "$0")"
 
-echo "Executing Homebrew Brewfile..."
-if [ -f "$HOME/Brewfile" ]; then
-	echo "Using $HOME/Brewfile"
-	brew bundle --file="$HOME/Brewfile"
-else
-	echo "Using gdot's Brewfile"
-	brew bundle --file="$GDOT_HOME/macos/Brewfile"
-fi
+brew bundle --file="$GDOT_HOME/linux/common/Brewfile"
+brew bundle --file="$GDOT_HOME/macos/Brewfile"
